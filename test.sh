b08f71da3adc29e36c87784d5eb6a61d9224e5d4
Zdeněk Lapeš <lapes.zdenek@gmail.com>
2024-04-15 22:08:02 +0200

    improve test.sh

--- test.sh
+++ test.sh
@@ -18,9 +18,17 @@ function build_mpi() {
 }
 
 function run_mpi() {
-    proc=6 # accept any number of processes available on the system
+    rows_in_file=$(wc -l < "$game_input")
+    available_proceses=$(nproc)
+    if [ "$rows_in_file" -gt "$available_proceses" ]; then
+        proc="$available_proceses"
+    else
+        proc="$rows_in_file"
+    fi
+#    proc=6 # accept any number of processes available on the system
     build_mpi || die "build failed"
     program="life"
+
 #    echo "Pocet procesoru: $proc; pocet kroku: $steps"
 #    echo "================"
     mpirun -np "$proc" ./"$program" "$game_input" "$steps" || die "mpirun failed"
